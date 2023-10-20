fc27bf6006979c73ff43b56266386fc3e8df83a5
Solihudin15 <zsolihudin0215@gmail.com>
2023-10-20 23:38:39 +0700

    New File

--- sesi27.php
+++ sesi27.php
@@ -3,6 +3,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tugas Sesi 27</title>
+    <style>
+        table, tr, th, td{
+            border: 2px solid black;
+            border-collapse: collapse;
+        }
+    </style>
 </head>
 <body>
     <!-- Tugas Nomer 1 -->
@@ -16,23 +22,30 @@
     ?>
     <br><hr><br>
     <!-- Tugas Nomer 2 -->
-    <table border="1">
-        <tr style="background-color: cyan;">
+    <table>
+        <tr style ="background-color: cyan;">
             <td>No</td>
             <td>Nama</td>
             <td>Kelas</td>
         </tr>
         <?php
-        for ($i = 1, $a = 10; $i <=10, $a >= 1; $i++, $a--){
-        ?>
-            <tr>
-                <td><?php echo "$i" ?></td>
-                <td><?php echo "Nama ke $i" ?></td>
-                <td><?php echo "Kelas $a"?></td>
-            </tr>
-        <?php
+        for ($i = 1, $a = 10; $i <= 10, $a >= 1; $i++, $a--){
+            if ($i % 2 == 0){
+            echo "<tr style ='background-color: white'>";
+                echo "<td>$i</td>";
+                echo "<td>Nama ke $i</td>";
+                echo "<td>Kelas $a</td>";
+            echo "</tr>";
+            } else{
+
+            echo"<tr style ='background-color: gray;'>";
+                echo"<td>$i</td>";
+                echo"<td>Nama ke $i</td>";
+                echo"<td>Kelas $a</td>";
+            echo "</tr>";
+            }
         }
-        ?>
+            ?>
     </table>
 </body>
 </html>
