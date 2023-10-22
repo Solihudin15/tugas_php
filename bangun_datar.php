76a22967b6b7aea4cd81552f1005b5c3afaf6d88
Solihudin15 <zsolihudin0215@gmail.com>
2023-10-22 11:01:21 +0700

    New File

---
+++ bangun_datar.php
@@ -0,0 +1,63 @@
+<?php
+//                                  Menghitung Luas Persegi
+echo "Menghitung Luas Persegi <br><br>";
+
+$s = 9;
+$luas = $s * $s;
+
+echo "Sebuah Persegi Mempunyai sisi $s cm <br>";
+echo "Rumus Luas Persegi : sisi x sisi <br>";
+echo "Luas dari Persegi yang memiliki $s cm adalah $luas";
+echo "<br><br><hr>";
+
+//                                 Menghitung persegi Panjang 
+echo "Menghitung Luas Persegi Panjang <br><br>";
+
+$p = 18;
+$l = 8;
+$luas = $p * $l;
+
+echo "Sebuah Persegi panjang mempunyai panjang $p cm dan lebar $l cm <br>";
+echo "Rumus Luas Persegi Panjang : panjang x lebar <br>";
+echo "Luas dari Persegi yang memiliki panjang $p cm dan lebar $l cm adalah $luas";
+echo "<br><br><hr>";
+
+
+//                                 Menghitung Luas Lingkaran
+echo "Menghitung Luas Lingkaran <br><br>";
+
+$phi = 3.14;
+$r = 12;
+$luas = $phi * $r * $r;
+
+echo "Sebuah lingkaran mempunyai jari-jari $r cm <br>";
+echo "Rumus Luas lingkaran : phi x jari-jari x jari-jari <br>";
+echo "Luas dari lingkaran yang memiliki jari-jari $r cm adalah $luas";
+echo "<br><br><hr>";
+
+
+//                                 Menghitung Luas Segitiga
+echo "Menghitung Luas Segitiga <br><br>";
+
+$a = 6;
+$t = 9;
+$luas = 0.5 * $a * $t;
+
+echo "Sebuah Segitiga mempunyai alas $a cm dan tinggi $t cm <br>";
+echo "Rumus Luas Segitiga : 1/2 * alas * tinggi <br>";
+echo "Luas dari Segitiga yang memiliki alas $a cm dan tinggi $t cm adalah $luas";
+echo "<br><br><hr>";
+
+//                                 Menghitung Luas jajar Genjang
+echo "Menghitung Luas Jajar genjang <br><br>";
+
+$a = 12;
+$t = 8;
+$luas = $a * $t;
+
+echo "Sebuah Jajar Genjang mempunyai alas $a cm dan tinggi $t cm <br>";
+echo "Rumus Luas Jajar Genjang : alas * tinggi <br>";
+echo "Luas dari jajar Genjang yang memiliki alas $a cm dan tinggi $t cm adalah $luas";
+echo "<br><br><hr>";
+?>
+
