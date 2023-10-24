d4c1e6afe21008df58522974f71ee5ae72f3dfbc
Solihudin15 <zsolihudin0215@gmail.com>
2023-10-24 17:24:37 +0700

    New File

--- bangun_ruang.php
+++ bangun_ruang.php
@@ -1,40 +1,46 @@
-<?php
-//                          Menghitung Volume Kubus
-echo "Menghitung Volume Kubus <br><br>";
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Bangun Ruang</title>
+</head>
+<body>
+    <?php
+    //                          Menghitung Volume Kubus
+    echo "Menghitung Volume Kubus <br><br>";
 
-$s = 10;
-$volume = $s * $s * $s;
+    $s = 10;
+    $volume = $s * $s * $s;
 
-echo "Sebuah Kubus Mempunyai sisi $s cm <br>";
-echo "Rumus Volume Kubus : sisi x sisi x sisi <br>";
-echo "Volume dari Persegi yang memiliki $s cm adalah $volume";
-echo "<br><br><hr>";
+    echo "Sebuah Kubus Mempunyai sisi $s cm <br>";
+    echo "Rumus Volume Kubus : sisi x sisi x sisi <br>";
+    echo "Volume dari Persegi yang memiliki $s cm adalah $volume";
+    echo "<br><br><hr>";
 
+    //                          Menghitung Volume Balok
+    echo "Menghitung Volume Balok <br><br>";
 
-//                          Menghitung Volume Balok
+    $p = 10;
+    $l = 5;
+    $t = 3;
+    $volume = $p * $l * $t;
 
-echo "Menghitung Volume Balok <br><br>";
+    echo "Sebuah Balok Mempunyai Panjang $p cm, Lebar $l cm, dan Tinggi $t cm <br>";
+    echo "Rumus Volume Balok : Panjang x Lebar x Tinggi <br>";
+    echo "Volume dari balok yang memiliki Panjang $p cm, Lebar $l cm, dan Tinggi $t cm adalah $volume";
+    echo "<br><br><hr>";
 
-$p = 10;
-$l = 5;
-$t = 3;
-$volume = $p * $l * $t;
+    //                          Menghitung Volume Prisma Segitiga
+    echo "Menghitung Volume Prisma Segitiga <br><br>";
 
-echo "Sebuah Balok Mempunyai Panjang $p cm, Lebar $l cm, dan Tinggi $t cm <br>";
-echo "Rumus Volume Balok : Panjang x Lebar x Tinggi <br>";
-echo "Volume dari balok yang memiliki Panjang $p cm, Lebar $l cm, dan Tinggi $t cm adalah $volume";
-echo "<br><br><hr>";
+    $la = 30;
+    $t = 10;
+    $volume = $la * $t;
 
-//                          Menghitung Volume Prisma Segitiga
-
-echo "Menghitung Volume Prisma Segitiga <br><br>";
-
-$la = 30;
-$t = 10;
-$volume = $la * $t;
-
-echo "Sebuah Prisma Segitiga Mempunyai Luas Alas $la cm dan Tinggi $t cm <br>";
-echo "Rumus Volume Prisma Segitiga : Luas Alas x Tinggi <br>";
-echo "Volume dari Prisma Segitiga yang Luas Alas $la cm dan Tinggi $t cm adalah $volume";
-echo "<br><br><hr>";
-?>
+    echo "Sebuah Prisma Segitiga Mempunyai Luas Alas $la cm dan Tinggi $t cm <br>";
+    echo "Rumus Volume Prisma Segitiga : Luas Alas x Tinggi <br>";
+    echo "Volume dari Prisma Segitiga yang Luas Alas $la cm dan Tinggi $t cm adalah $volume";
+    echo "<br><br><hr>";
+    ?>
+</body>
+</html>
---
+++ kategori.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Body mass Index</title>
+</head>
+<body>
+    <?php
+    //                          Menghitung Body Mass Index
+    $nama = "Solihudin";
+    $tinggi = 170;                                     // Tinggi dalam satuan centimeter
+    $berat = 70;
+
+    $tinggim = $tinggi / 100;                          // Rumus konvesi dari centimeter ke meter 
+    $rumus = ( $berat / ( $tinggim * $tinggim ) );     // Rumus untuk Body Mass Index
+    $bmi = number_format($rumus,1);                    // Untuk memformat nilai koma menjadi 1 digit      
+
+    if ( $bmi < 18.5 ){
+        echo "Halo, $nama. Anda memiliki Tinggi $tinggi cm dan Berat $berat Kg, Nilai BMI anda adalah $bmi, anda termasuk Kurus";
+    }elseif ( $bmi >= 18.5 && $bmi <= 24.9 ) {
+        echo "Halo, $nama. Anda memiliki Tinggi $tinggi cm dan Berat $berat Kg, Nilai BMI anda adalah $bmi, anda termasuk Ideal";
+    }else {
+        echo "Halo, $nama. Anda memiliki Tinggi $tinggi cm dan Berat $berat Kg, Nilai BMI anda adalah $bmi, anda termasuk Gemuk";
+    }
+    ?>
+</body>
+</html>
