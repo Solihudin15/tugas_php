ba366b010f05c93fcc09b71943321f216f3216b4
Solihudin15 <zsolihudin0215@gmail.com>
2023-10-29 11:58:16 +0700

    New File

--- sesi29/index.php
+++ sesi29/index.php
@@ -8,20 +8,44 @@ $query = mysqli_query($connection, "SELECT * FROM anggota");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Keanggotaan Perpustakaan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
-        header{
+        #navbar{
+            display: flex;
             background-color: skyblue;
-            text-align: center;
+            padding: 7px;
+            margin-bottom: 10px;
         }
-        h3 {
+        #navbar a{
+            text-decoration: none;
+        }
+        #logo{
+            font-weight: 700;
+            font-size: 30px;
+        }
+        #logo a{
+            color: red;
+        }
+        #logo a span{
+            color: white;
+        }
+        #menu {
+            margin-top: 10px;
+            font-weight: 500;
+            font-size: 18px;
+        }
+        #menu a{
+            color: white;
+        }
+        h4 {
+            text-align: center;
             padding-top: 10px;
             padding-left: 20px;
-            padding-bottom: 10px;
         }
         table{
             border: 2px solid black;
             border-collapse: collapse;
-            margin-top: 30px;
+            margin-bottom: 50px;
         }
         tr, td{
             text-align: center;
@@ -32,48 +56,59 @@ $query = mysqli_query($connection, "SELECT * FROM anggota");
     </style>
 </head>
 <body>
-<header>
-    <h3>Data Keanggotaan Perpustakaan</h3>
-</header>
-    <table align="center">
-        <tr style="background-color: cyan;">
-            <td style="text-align: center; font-weight: 700;">No.</td>
-            <td style="text-align: center; font-weight: 700;">Nama</td>
-            <td style="text-align: center; font-weight: 700;">Jenis Kelamin</td>
-            <td style="text-align: center; font-weight: 700;">No.Telp</td>
-            <td style="text-align: center; font-weight: 700;">Alamat</td>
-            <td style="text-align: center; font-weight: 700;">Email</td>
-            <td style="text-align: center; font-weight: 700;">Terdaftar Sejak</td>
-            <td style="text-align: center; font-weight: 700;">Role</td>
-        </tr>
-        <?php
-        $no = 1;
-        while($anggota = mysqli_fetch_array($query)){ 
-            if ($no % 2 == 0) { ?>
-                <tr style="background-color: lightgrey;">
-                    <td style="text-align: center;"><?php echo $no ?></td>
-                    <td><?php echo $anggota["nama"] ?></td>
-                    <td style="text-align: center;"><?php echo $anggota["sex"] ?></td>
-                    <td><?php echo $anggota["telp"] ?></td>
-                    <td><?php echo $anggota["alamat"] ?></td>
-                    <td><?php echo $anggota["email"] ?></td>
-                    <td><?php echo $anggota["tgl_entry"] ?></td>
-                    <td style="text-align: center;"><?php echo $anggota["role"] ?></td>
+    <!-- Navbar -->
+    <div id="navbar">
+        <div class="col-md-1"></div>
+        <div id="logo" class="col-md-7"><a href="index.php">Perpustakaan<span>Kita</span></a></div>
+        <div id="menu" class="col-md-1 text-center"><a href="index.php">Anggota</a></div>
+        <div id="menu" class="col-md-1 text-center"><a href="buku.php">Buku</a></div>
+        <div id="menu" class="col-md-1 text-center"><a href="peminjaman.php">Peminjaman</a></div>
+        <div class="col-md-1"></div>
+    </div>
+    <div id="container2" class="container">
+        <div class="col-md-12">
+            <h4>Data Keanggotaan Perpustakaan</h4>
+            <table align="center">
+                <tr style="background-color: cyan;">
+                    <td style="text-align: center; font-weight: 700;">No.</td>
+                    <td style="text-align: center; font-weight: 700;">Nama</td>
+                    <td style="text-align: center; font-weight: 700;">Jenis Kelamin</td>
+                    <td style="text-align: center; font-weight: 700;">No.Telp</td>
+                    <td style="text-align: center; font-weight: 700;">Alamat</td>
+                    <td style="text-align: center; font-weight: 700;">Email</td>
+                    <td style="text-align: center; font-weight: 700;">Terdaftar Sejak</td>
+                    <td style="text-align: center; font-weight: 700;">Role</td>
                 </tr>
-            <?php } else {?>
-                <tr style="background-color: white;">
-                    <td style="text-align: center;"><?php echo $no ?></td>
-                    <td><?php echo $anggota["nama"] ?></td>
-                    <td style="text-align: center;"><?php echo $anggota["sex"] ?></td>
-                    <td><?php echo $anggota["telp"] ?></td>
-                    <td><?php echo $anggota["alamat"] ?></td>
-                    <td><?php echo $anggota["email"] ?></td>
-                    <td><?php echo $anggota["tgl_entry"] ?></td>
-                    <td style="text-align: center;"><?php echo $anggota["role"] ?></td>
-                </tr>
-                <?php } ?>
-        <?php $no++ ?>
-        <?php } ?>
-    </table>
+                    <?php
+                    $no = 1;
+                    while($anggota = mysqli_fetch_array($query)){ 
+                        if ($no % 2 == 0) { ?>
+                            <tr style="background-color: lightgrey;">
+                                <td style="text-align: center;"><?php echo $no ?></td>
+                                <td><?php echo $anggota["nama"] ?></td>
+                                <td style="text-align: center;"><?php echo $anggota["sex"] ?></td>
+                                <td><?php echo $anggota["telp"] ?></td>
+                                <td><?php echo $anggota["alamat"] ?></td>
+                                <td><?php echo $anggota["email"] ?></td>
+                                <td><?php echo $anggota["tgl_entry"] ?></td>
+                                <td style="text-align: center;"><?php echo $anggota["role"] ?></td>
+                            </tr>
+                        <?php } else {?>
+                            <tr style="background-color: white;">
+                                <td style="text-align: center;"><?php echo $no ?></td>
+                                <td><?php echo $anggota["nama"] ?></td>
+                                <td style="text-align: center;"><?php echo $anggota["sex"] ?></td>
+                                <td><?php echo $anggota["telp"] ?></td>
+                                <td><?php echo $anggota["alamat"] ?></td>
+                                <td><?php echo $anggota["email"] ?></td>
+                                <td><?php echo $anggota["tgl_entry"] ?></td>
+                                <td style="text-align: center;"><?php echo $anggota["role"] ?></td>
+                            </tr>
+                            <?php } ?>
+                    <?php $no++ ?>
+                    <?php } ?>
+                </table>
+        </div>
+    </div>
 </body>
 </html>
